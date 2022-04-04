471b9a7a542a61ff33387b9a2d43d201c05553b0
alexlember <alexlember@gmail.com>
2022-04-04 20:51:59 +0200

    Basic operator connect

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Keyboar
 from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler, \
     MessageHandler, Filters
 from settings import TELEGRAM_TOKEN, HELLO_MESSAGE, READY_TO_HELP_BTN_TXT, NEED_HELP_BTN_TXT, NEED_HELP_INFORMATION, \
-    READY_TO_HELP_INFORMATION, CONNECT_TO_VOLUNTEER_INFORMATION, CONNECT_TO_VOLUNTEER_BTN_TXT
+    READY_TO_HELP_INFORMATION, CONNECT_TO_VOLUNTEER_INFORMATION, CONNECT_TO_VOLUNTEER_BTN_TXT, \
+    REPLY_TO_THIS_MESSAGE_TXT, WRONG_REPLY_TXT, SUPPORT_CHAT_ID
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -55,6 +56,48 @@ def ready_to_help(update: Update, context: CallbackContext) -> None:
 def connect_with_operators(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(CONNECT_TO_VOLUNTEER_INFORMATION)
 
+    user_info = update.message.from_user.to_dict()
+
+    context.bot.send_message(
+        chat_id=SUPPORT_CHAT_ID,
+        text=f"""
+📞 Connected {user_info}.
+        """,
+    )
+
+
+def forward_to_support_chat(update, context):
+
+    forwarded = update.message.forward(chat_id=SUPPORT_CHAT_ID)
+    if not forwarded.forward_from:
+        context.bot.send_message(
+            chat_id=SUPPORT_CHAT_ID,
+            reply_to_message_id=forwarded.message_id,
+            text=f"{update.message.from_user.id}\n{REPLY_TO_THIS_MESSAGE_TXT}"
+        )
+
+
+def forward_from_support_to_user(update, context):
+    user_id = None
+    if update.message.reply_to_message.forward_from:
+        user_id = update.message.reply_to_message.forward_from.id
+    elif REPLY_TO_THIS_MESSAGE_TXT in update.message.reply_to_message.text:
+        try:
+            user_id = int(update.message.reply_to_message.text.split('\n')[0])
+        except ValueError:
+            user_id = None
+    if user_id:
+        context.bot.copy_message(
+            message_id=update.message.message_id,
+            chat_id=user_id,
+            from_chat_id=update.message.chat_id
+        )
+    else:
+        context.bot.send_message(
+            chat_id=SUPPORT_CHAT_ID,
+            text=WRONG_REPLY_TXT
+        )
+
 
 updater = Updater(TELEGRAM_TOKEN)
 
@@ -63,12 +106,15 @@ updater.dispatcher.add_handler(CallbackQueryHandler(button))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(Filters.regex('^(' + NEED_HELP_BTN_TXT + ')$'), need_help))
 updater.dispatcher.add_handler(MessageHandler(Filters.regex('^(' + READY_TO_HELP_BTN_TXT + ')$'), ready_to_help))
-updater.dispatcher.add_handler(MessageHandler(Filters.regex('^(' + CONNECT_TO_VOLUNTEER_BTN_TXT + ')$'), connect_with_operators))
+updater.dispatcher.add_handler(MessageHandler(Filters.regex('^(' + CONNECT_TO_VOLUNTEER_BTN_TXT + ')$'),
+                                              connect_with_operators))
 updater.dispatcher.add_handler(CommandHandler('oncall', on_call))
 updater.dispatcher.add_handler(CommandHandler('offcall', off_call))
 updater.dispatcher.add_handler(CommandHandler('readytohelp', ready_to_help))
 updater.dispatcher.add_handler(CommandHandler('needhelp', need_help))
-# updater.dispatcher.add_handler(conv_handler)
+
+updater.dispatcher.add_handler(MessageHandler(Filters.chat_type.private, forward_to_support_chat))
+updater.dispatcher.add_handler(MessageHandler(Filters.chat(SUPPORT_CHAT_ID) & Filters.reply, forward_from_support_to_user))
 
 
 updater.start_polling()
--- settings.py
+++ settings.py
@@ -7,11 +7,18 @@ TELEGRAM_TOKEN = os.getenv("REFUGEE_TELEGRAM_TOKEN")
 if TELEGRAM_TOKEN is None:
     raise Exception("Please setup the .env variable REFUGEE_TELEGRAM_TOKEN.")
 
+SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID"))
+if SUPPORT_CHAT_ID is None:
+    raise Exception("Please setup the .env variable SUPPORT_CHAT_ID.")
+
 HELLO_MESSAGE = """
 Телеграм бот “Russians against war” в Швеции поможет собрать полезную информацию и легко ее найти (беженцам), а заодно предоставит быстрый и оперативный канал для связи.
 Информацию можно будет получить по определенным разделам, нажимая на кнопки.
 Связаться с нами и задать вопрос можно будет через кнопку “Связаться с координаторами”, несколько волонтеров будут время от времени проверять, поступили ли сообщения в бот, и реагировать на них. 
 """
+
+REPLY_TO_THIS_MESSAGE_TXT = "Reply на это сообщение"
+WRONG_REPLY_TXT = "Ошибка при Reply (не то сообщение)"
 
 READY_TO_HELP_BTN_TXT = "Хочу помочь"
 NEED_HELP_BTN_TXT = "Мне нужна помощь"
